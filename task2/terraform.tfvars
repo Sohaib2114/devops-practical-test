35e6d686f4e5f59f3735d20112543c568aeaa50c
sohaib2114 <sohaib2114@gmail.com>
2025-03-28 09:24:55 +0500

    new task

---
+++ task2/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region = "us-west-2"
+ami_id     = "ami-830c94e3"
+
---
+++ task2/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "ami_id" {
+  description = "ami-id"
+  type        = string
+}
+
